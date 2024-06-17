84e91faf1a8ca2119fd2435b3221147f8621ac53
User <User@W11PC>
2024-06-17 10:39:53 +0300

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,33 @@
+﻿class Program
+{
+    static void Main(string[] args)
+    {
+        long maxMemory = 1000000;
+        List<long> list = new List<long>();
+        long i = 0;
+
+        while (true)
+        {
+            GetMessage();
+            list.Add(i);
+            i++;
+        }
+
+        void GetMessage()
+        {
+            long memory = GetCurrentMemory();
+            if (memory > maxMemory)
+            {
+                Console.WriteLine("Внимание потребление ресурсов вышло за приделы - " + memory + " байт");
+            }
+            else
+            {
+                Console.WriteLine("Текущее потребление ресурсов - " + memory + " байт");
+            }
+        }
+        long GetCurrentMemory()
+        {
+            return GC.GetTotalMemory(true);
+        }
+    }
+}
